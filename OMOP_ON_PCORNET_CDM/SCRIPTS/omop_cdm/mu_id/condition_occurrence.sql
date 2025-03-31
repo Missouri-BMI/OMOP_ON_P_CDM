f1451cc7b9f3052fca6f013f6c67931fb63671c3
Vasanthi Mandhadi <vmfz5@mail.missouri.edu>
2025-03-31 14:45:54 -0500

    labs added to measurement and changes made to condition occurrence

--- OMOP_ON_PCORNET_CDM/SCRIPTS/omop_cdm/mu_id/condition_occurrence.sql
+++ OMOP_ON_PCORNET_CDM/SCRIPTS/omop_cdm/mu_id/condition_occurrence.sql
@@ -1,68 +1,55 @@
 create or replace view {cdm_db}.{cdm_schema}.condition_occurrence as
-
 SELECT
     diagnosis.diagnosisid::INTEGER AS condition_occurrence_id,
     diagnosis.patid::INTEGER AS person_id,
-    coalesce(case
-        when diagnosis.dx_type='09' then c_icd9.concept_id
-        when diagnosis.dx_type='10' then c_icd10.concept_id
-        when diagnosis.dx_type='SM' then c_snomed.concept_id
-    else
-     0
-    end,44814650)::INTEGER as condition_concept_id,
+    COALESCE(target_concept.concept_id, 44814650)::INTEGER AS condition_concept_id,
     coalesce(diagnosis.dx_date,diagnosis.admit_date)::DATE AS condition_start_date,
     coalesce(diagnosis.dx_date,diagnosis.admit_date)::timestamp AS condition_start_datetime,
     NULL::DATE AS condition_end_date,
     NULL::timestamp AS condition_end_datetime,
     case
-        when enc_type='ED' and dx_origin='BI' and pdx='P'then 2000001282
-        when enc_type='ED' and dx_origin='OD' and pdx='P'then 2000001280
-        when enc_type='ED' and dx_origin='CL' and pdx='P'then 2000001281
-        when enc_type='ED' and dx_origin='BI' and pdx='S'then 2000001284
-        when enc_type='ED' and dx_origin='OD' and pdx='S'then 2000001283
-        when enc_type='ED' and dx_origin='CL' and pdx='S'then 2000001285
-        when enc_type in ('AV','OA','TH') and dx_origin='BI' and pdx='P'then 2000000096
-        when enc_type in ('AV','OA','TH') and dx_origin='OD' and pdx='P'then 2000000095
-        when enc_type in ('AV','OA','TH') and dx_origin='CL' and pdx='P'then 2000000097
-        when enc_type in ('AV','OA','TH') and dx_origin='BI' and pdx='S'then 2000000102
-        when enc_type in ('AV','OA','TH') and dx_origin='OD' and pdx='S'then 2000000101
-        when enc_type in ('AV','OA','TH') and dx_origin='CL' and pdx='S'then 2000000103
-        when enc_type in ('IP','OS','IS','EI') and dx_origin='BI' and pdx='P'then 2000000093
-        when enc_type in ('IP','OS','IS','EI') and dx_origin='OD' and pdx='P'then 2000000092
-        when enc_type in ('IP','OS','IS','EI') and dx_origin='CL' and pdx='P'then 2000000094
-        when enc_type in ('IP','OS','IS','EI') and dx_origin='BI' and pdx='S'then 2000000099
-        when enc_type in ('IP','OS','IS','EI') and dx_origin='OD' and pdx='S'then 2000000098
-        when enc_type in ('IP','OS','IS','EI') and dx_origin='CL' and pdx='S'then 2000000100
-    else
-        44814650
-    end::INTEGER  as condition_type_concept_id,
-    4230359::INTEGER AS condition_status_concept_id,   
-    /*
-    case  
-    when pdx = 'P' then 32902 
-    when pdx = 'S' then 32908
-    when pdx = 'P' and DX_SOURCE = 'DI' then 32903
-     when pdx = 'S' and DX_SOURCE = 'DI' then 32909  AS condition_status_concept_id,
-*/
+            when dx_origin = 'OD' then 32817::int
+            when dx_origin = 'BI' then 32821
+            when dx_origin = 'CL' then 32810
+            when dx_origin = 'DR' then 45754907
+            when dx_origin = 'NI' then 44814650
+            when dx_origin = 'UN' then 44814653
+            when dx_origin = 'OT' then 44814649
+            when dx_origin = '' then 44814653
+            when dx_origin is null then 44814653
+        end::INTEGER  as condition_type_concept_id,
+    case
+            when dx_source = 'AD' then 32890::int
+            when dx_source = 'DI' then 32896
+            when dx_source = 'FI' then 40492206
+            when dx_source = 'IN' then 40492208
+            when dx_source = 'NI' then 44814650
+            when dx_source = 'UN' then 44814653
+            when dx_source = 'OT' then 44814649
+            when dx_source = '' then 44814653
+            when dx_source is null then 44814653
+        end::INTEGER AS condition_status_concept_id,   
     NULL::varchar(20) AS stop_reason,
     diagnosis.providerid::INTEGER AS provider_id,
     diagnosis.encounterid::INTEGER AS visit_occurrence_id,
     NULL::INTEGER AS visit_detail_id,
-    diagnosis.dx::varchar(50) AS condition_source_value,
-    coalesce(case
-        when diagnosis.dx_type='09' then c_icd9.concept_id
-        when diagnosis.dx_type='10' then c_icd10.concept_id
-        when diagnosis.dx_type='SM' then c_snomed.concept_id
-    else
-        0
-    end,44814650)::INTEGER as condition_source_concept_id,
+    'dx_icd_type:' || diagnosis.dx_type::varchar(50) || '|' || dx || ':' || diagnosis.dx::varchar(50) AS condition_source_value,
+   COALESCE(source_concept.concept_id, 44814650)::INTEGER AS condition_source_concept_id,
     diagnosis.dx_source::varchar(50) AS condition_status_source_value
 
 FROM {pcornet_db}.{pcornet_schema}.diagnosis diagnosis
-left join {cdm_db}.{vocabulary}.concept c_icd9 on diagnosis.dx=c_icd9.concept_code
-    and c_icd9.vocabulary_id='ICD9CM' and diagnosis.dx_type='09'
-left join {cdm_db}.{vocabulary}.concept c_icd10 on diagnosis.dx=c_icd10.concept_code
-    and c_icd10.vocabulary_id='ICD10CM' and diagnosis.dx_type='10'
-left join {cdm_db}.{vocabulary}.concept c_snomed on diagnosis.dx=c_snomed.concept_code
-    and c_snomed.vocabulary_id='SNOMED' and diagnosis.dx_type='SM'
+LEFT JOIN {cdm_db}.{cdm_schema}.concept source_concept 
+    ON diagnosis.dx = source_concept.concept_code
+    AND (
+        (diagnosis.dx_type = '09' AND source_concept.vocabulary_id = 'ICD9CM') OR
+        (diagnosis.dx_type = '10' AND source_concept.vocabulary_id = 'ICD10CM') 
+    )
+LEFT JOIN {cdm_db}.{cdm_schema}.concept_relationship cr
+    ON source_concept.concept_id = cr.concept_id_1
+    AND cr.relationship_id = 'Maps to'
+    AND (cr.invalid_reason IS NULL or cr.invalid_reason = '')
+LEFT JOIN {cdm_db}.{cdm_schema}.concept target_concept
+    ON cr.concept_id_2 = target_concept.concept_id
+    AND target_concept.standard_concept = 'S'
+    AND (target_concept.invalid_reason IS NULL or target_concept.invalid_reason = '');
 ;
--- OMOP_ON_PCORNET_CDM/SCRIPTS/omop_cdm/mu_id/measurement.sql
+++ OMOP_ON_PCORNET_CDM/SCRIPTS/omop_cdm/mu_id/measurement.sql
@@ -1,51 +1,106 @@
 create or replace view {cdm_db}.{cdm_schema}.measurement
-AS
-SELECT
-   lab.lab_result_cm_id::INTEGER AS measurement_id,
+(
+	MEASUREMENT_ID,
+	PERSON_ID,
+	MEASUREMENT_CONCEPT_ID,
+	MEASUREMENT_DATE,
+	MEASUREMENT_DATETIME,
+	MEASUREMENT_TIME,
+	MEASUREMENT_TYPE_CONCEPT_ID,
+	OPERATOR_CONCEPT_ID,
+	VALUE_AS_NUMBER,
+	VALUE_AS_CONCEPT_ID,
+	UNIT_CONCEPT_ID,
+	RANGE_LOW,
+	RANGE_HIGH,
+	PROVIDER_ID,
+	VISIT_OCCURRENCE_ID,
+	VISIT_DETAIL_ID,
+	MEASUREMENT_SOURCE_VALUE,
+	MEASUREMENT_SOURCE_CONCEPT_ID,
+	UNIT_SOURCE_VALUE,
+	UNIT_SOURCE_CONCEPT_ID,
+	VALUE_SOURCE_VALUE,
+	MEASUREMENT_EVENT_ID,
+	MEAS_EVENT_FIELD_CONCEPT_ID
+) as 
 
-   lab.patid::INTEGER AS person_id,
-
-   NULL::INTEGER AS measurement_concept_id,
-
-   NULL::DATE AS measurement_date,
-
-   NULL::TIMESTAMP AS measurement_datetime,
-
-   NULL::VARCHAR(10) AS measurement_time,
-
-   NULL::INTEGER AS measurement_type_concept_id,
-
-   NULL::INTEGER AS operator_concept_id,
-
-   NULL::NUMERIC AS value_as_number,
-
-   NULL::INTEGER AS value_as_concept_id,
-
-   NULL::INTEGER AS unit_concept_id,
-
-   NULL::NUMERIC AS range_low,
-
-   NULL::NUMERIC AS range_high,
-
-   NULL::INTEGER AS provider_id,
-
-   lab.encounter_num::INTEGER AS visit_occurrence_id,
-
-   NULL::INTEGER AS visit_detail_id,
-
-   NULL::VARCHAR(50) AS measurement_source_value,
-
-   NULL::INTEGER AS measurement_source_concept_id,
-
-   NULL::VARCHAR(50) AS unit_source_value,
-
-   NULL::INTEGER AS unit_source_concept_id,
-
-   NULL::VARCHAR(50) AS value_source_value,
-
-   NULL::INTEGER AS measurement_event_id,
-
-   NULL::INTEGER AS meas_event_field_concept_id
-
-FROM {pcornet_db}.{pcornet_schema}.lab_result_cm lab
+SELECT distinct
+            lab.lab_result_cm_id::INTEGER   AS measurement_id,
+            lab.patid::INTEGER    AS person_id,
+            c.concept_id::INTEGER     AS measurement_concept_id, --concept id for lab - measurement
+            lab.result_date::DATE          AS measurement_date,
+            lab.result_date::TIMESTAMP          AS measurement_datetime,
+            lab.result_time::VARCHAR(10)          AS measurement_time, 
+            nvl(c_result.concept_id, 0)::INTEGER AS measurement_type_concept_id,
+            NULL::INTEGER AS operator_concept_id,
+            lab.result_num::NUMERIC           AS value_as_number, --result_num
+            CASE
+                WHEN lower(TRIM(result_qual)) = 'positive'             THEN
+                    45884084
+                WHEN lower(TRIM(result_qual)) = 'negative'             THEN
+                    45878583
+                WHEN lower(TRIM(result_qual)) = 'pos'                  THEN
+                    45884084
+                WHEN lower(TRIM(result_qual)) = 'neg'                  THEN
+                    45878583
+                WHEN lower(TRIM(result_qual)) = 'presumptive positive' THEN
+                    45884084
+                WHEN lower(TRIM(result_qual)) = 'presumptive negative' THEN
+                    45878583
+                WHEN lower(TRIM(result_qual)) = 'detected'             THEN
+                    45884084
+                WHEN lower(TRIM(result_qual)) = 'not detected'         THEN
+                    45878583
+                WHEN lower(TRIM(result_qual)) = 'inconclusive'         THEN
+                    45877990
+                WHEN lower(TRIM(result_qual)) = 'normal'               THEN
+                    45884153
+                WHEN lower(TRIM(result_qual)) = 'abnormal'             THEN
+                    45878745
+                WHEN lower(TRIM(result_qual)) = 'low'                  THEN
+                    45881666
+                WHEN lower(TRIM(result_qual)) = 'high'                 THEN
+                    45876384
+                WHEN lower(TRIM(result_qual)) = 'borderline'           THEN
+                    45880922
+                WHEN lower(TRIM(result_qual)) = 'elevated'             THEN
+                    4328749  --ssh add issue number 55 - 6/26/2020 
+                WHEN lower(TRIM(result_qual)) = 'undetermined'         THEN
+                    45880649
+                WHEN lower(TRIM(result_qual)) = 'undetectable'         THEN
+                    45878583 
+                WHEN lower(TRIM(result_qual)) IN (
+                    'ni',
+                    'ot',
+                    'un',
+                    'no information',
+                    'unknown',
+                    'other'
+                ) THEN
+                    NULL::INTEGER
+                WHEN result_qual IS NULL THEN
+                    NULL::INTEGER
+                ELSE
+                    45877393::INTEGER
+            END AS value_as_concept_id,
+            u.concept_id::INTEGER     AS unit_concept_id,
+            TRY_CAST(lab.NORM_RANGE_LOW AS FLOAT)::NUMERIC as RANGE_LOW, 
+            TRY_CAST(lab.NORM_RANGE_HIGH AS FLOAT)::NUMERIC as RANGE_HIGH, -- non-numeric data will error on a insert
+            NULL::INTEGER AS provider_id,
+            lab.encounterid::INTEGER    AS visit_occurrence_id,
+            NULL::INTEGER AS visit_detail_id,
+            lab.lab_loinc::VARCHAR(50)            AS measurement_source_value,
+            0::INTEGER AS measurement_source_concept_id,
+            lab.result_unit::VARCHAR(50)          AS unit_source_value,
+            NULL::INTEGER AS unit_source_concept_id,
+            nvl(left(lab.raw_result,100), decode(lab.result_num, 0, lab.result_qual))::VARCHAR(100) AS value_source_value,
+            NULL::INTEGER AS measurement_event_id, NULL::INTEGER AS meas_event_field_concept_id
+        FROM
+            {pcornet_db}.{pcornet_schema}.lab_result_cm     lab
+            JOIN {cdm_db}.{cdm_schema}.concept c ON lab.lab_loinc = c.concept_code
+                                                            AND c.domain_id = 'Measurement'
+            LEFT JOIN {cdm_db}.{cdm_schema}.concept u ON lab.result_unit = u.concept_code
+                                                              --  AND u.src_cdm_column = 'RX_DOSE_ORDERED_UNIT'
+            LEFT JOIN {cdm_db}.{cdm_schema}.concept c_result ON lab.lab_result_source = c_result.concept_code;
 ;
