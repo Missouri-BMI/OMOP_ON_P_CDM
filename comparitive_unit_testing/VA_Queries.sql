d36c0dd64b6ce32e619056f63ad1acaa39a07b84
Preston Hunter <ph8c8@umsystem.edu>
2025-04-14 11:23:23 -0500

    replaced with appropriate omop script so ticket name matches data

--- comparitive_unit_testing/VA_Queries.sql
+++ comparitive_unit_testing/VA_Queries.sql
@@ -9,34 +9,38 @@ PCORNET_CDM.CDM.
 -- 1.1 Identify Patients with Visit in the last 5 Years
 DROP TABLE IF EXISTS Visits;
 CREATE TEMPORARY TABLE Visits as
-SELECT PATID, DISCHARGE_DATE AS VISIT_DATE --This is just VINCI Services way to grab Visit_Date, could also use VISIT_START_DATE
-FROM PCORNET_CDM.CDM.ENCOUNTER
-WHERE (DISCHARGE_DATE >= CAST(DATEADD(YEAR, -5, GETDATE()) AS date)
-OR DISCHARGE_DATE IS NULL) -- Start Date
-AND ADMIT_DATE < CAST(GETDATE() AS date) -- End Date
-AND ADMIT_DATE >= CAST('10/01/1999' AS date); -- Start of EHR TODO: is this needed?
+SELECT PERSON_ID, VISIT_END_DATE AS VISIT_DATE --This is just VINCI Services way to grab Visit_Date, could also use VISIT_START_DATE
+FROM OMOP_CDM.CDM.VISIT_OCCURRENCE
+WHERE (VISIT_END_DATE >= CAST(DATEADD(YEAR, -5, GETDATE()) AS date)
+OR VISIT_END_DATE IS NULL) -- Start Date
+AND VISIT_START_DATE < CAST(GETDATE() AS date) -- End Date
+AND VISIT_START_DATE >= CAST('10/01/1999' AS date); -- Start of EHR TODO: is this needed?
 
 -- 1.2 Get Demographics
 DROP TABLE IF EXISTS G7;
 CREATE TEMPORARY TABLE G7 as
-WITH Visit AS (SELECT PATID, MIN(VISIT_DATE) AS First_Vis
-FROM Visits GROUP BY PATID),
-Demo AS (SELECT DISTINCT V.PATID,First_Vis,BIRTH_DATE AS BirthDateTime,
-SEX,RACE AS Race,Hispanic AS Ethnicity
+WITH Visit AS (SELECT PERSON_ID, MIN(VISIT_DATE) AS First_Vis
+FROM Visits GROUP BY PERSON_ID),
+Demo AS (SELECT DISTINCT V.PERSON_ID,First_Vis,BIRTH_DATETIME AS BirthDateTime,
+GENDER_CONCEPT_ID as Gender,RACE_CONCEPT_ID AS Race,ETHNICITY_CONCEPT_ID AS Ethnicity
 FROM Visit AS V
-INNER JOIN PCORNET_CDM.CDM.DEMOGRAPHIC ON V.PATID = PCORNET_CDM.CDM.DEMOGRAPHIC.PATID)
-SELECT DISTINCT PATID, First_Vis, BirthDateTime, SEX, Race, Ethnicity
-FROM Demo;
+INNER JOIN OMOP_CDM.CDM.PERSON ON V.PERSON_ID = OMOP_CDM.CDM.PERSON.PERSON_ID)
+SELECT DISTINCT PERSON_ID, First_Vis, BirthDateTime, G.CONCEPT_NAME as Gender, R.CONCEPT_NAME as Race, E.CONCEPT_NAME as Ethnicity
+FROM Demo
+INNER JOIN OMOP_CDM.CDM.CONCEPT as G on Demo.Gender = G.CONCEPT_ID
+INNER JOIN OMOP_CDM.CDM.CONCEPT as R on Demo.Race = R.CONCEPT_ID
+INNER JOIN OMOP_CDM.CDM.CONCEPT as E on Demo.Ethnicity = E.CONCEPT_ID;
+
 -- 1.3 Save Row Cohort in FEAS and Get Distinct Patient Counts
 DROP TABLE IF EXISTS FEAS;
 CREATE TEMPORARY TABLE FEAS as
-SELECT DISTINCT PATID FROM G7;
-SELECT COUNT(DISTINCT PATID) FROM FEAS;
+SELECT DISTINCT PERSON_ID FROM G7;
+SELECT COUNT(DISTINCT PERSON_ID) FROM FEAS;
 -- 1.4 Breakdowns
 -- Gender
-SELECT SEX, COUNT(DISTINCT PATID) FROM G7 GROUP BY SEX ORDER BY SEX;
-SELECT Race, COUNT(DISTINCT PATID) FROM G7 GROUP BY Race ORDER BY Race;
-SELECT Ethnicity, COUNT(DISTINCT PATID) FROM G7 GROUP BY Ethnicity ORDER BY Ethnicity;
+SELECT Gender, COUNT(DISTINCT PERSON_ID) FROM G7 GROUP BY Gender ORDER BY Gender;
+SELECT Race, COUNT(DISTINCT PERSON_ID) FROM G7 GROUP BY Race ORDER BY Race;
+SELECT Ethnicity, COUNT(DISTINCT PERSON_ID) FROM G7 GROUP BY Ethnicity ORDER BY Ethnicity;
 /******************************** G_8 *********************************
             2. Number of patients from row 7 seen with type 2 diabetes diagnosis
             (ICD codes)
@@ -44,40 +48,50 @@ SELECT Ethnicity, COUNT(DISTINCT PATID) FROM G7 GROUP BY Ethnicity ORDER BY Ethn
 
 *************************************************************************/
 -- 2.2 Create ICD Dim Table
+
 DROP TABLE IF EXISTS Dim_ICD;
 CREATE TEMPORARY TABLE Dim_ICD as
-SELECT DISTINCT PATID, DX, ADMIT_DATE FROM PCORNET_CDM.CDM.DIAGNOSIS WHERE DX LIKE 'E11.%';
+SELECT DISTINCT
+PERSON_ID,
+CONDITION_START_DATE,
+CONDITION_END_DATE,
+CONDITION_CONCEPT_ID,
+CONDITION_SOURCE_VALUE
+FROM
+OMOP_CDM.CDM.CONDITION_OCCURRENCE
+WHERE
+(CONDITION_SOURCE_VALUE LIKE 'dx_icd_type:10|E11.%')
+AND (CONDITION_END_DATE >= CAST(DATEADD(YEAR, -5, GETDATE()) AS date) OR CONDITION_END_DATE IS NULL) -- Start Date
+AND CONDITION_START_DATE < CAST(GETDATE() AS date) -- End Date kind of pointless
+AND CONDITION_START_DATE >= CAST('10/01/1999' AS date);--TODO: icd type 9?
 
-DROP TABLE IF EXISTS TYPETWOPATIENTS;
-CREATE TEMPORARY TABLE TYPETWOPATIENTS as
-SELECT DISTINCT PATID FROM DIM_ICD;
 
 -- 2.3 Identify Patients With Diagnosis
 DROP TABLE IF EXISTS G8;
 CREATE TEMPORARY TABLE G8 as
-SELECT DISTINCT G7.PATID, RACE, SEX, Ethnicity, BIRTHDATETIME, FIRST_VIS FROM G7
-INNER JOIN TYPETWOPATIENTS AS C ON C.PATID = G7.PATID;
+SELECT DISTINCT G7.PERSON_ID, Race, Gender, Ethnicity,First_Vis,BIRTHDATETIME  from G7
+INNER JOIN DIM_ICD as C
+ON C.PERSON_ID = G7.PERSON_ID;
+
 --TODO: SHOULD first diagnosis date be used as substitute for CONDITION START DATE?
 -- AND CONDITION_START_DATE >= CAST('10/01/1999' AS date) -- Start of EHR
---Note that our date has no end or start date of a diagnosis, just an admit date. so we dont follow 1:1 the
--- process used by VA
 
 -- 2.4 Save Row Cohort in VINCI_Feasibility and Get Distinct Patient Counts
 DROP TABLE IF EXISTS FEAS1;
 CREATE TEMPORARY TABLE FEAS1 as
-SELECT DISTINCT PATID FROM G8;
-SELECT COUNT(DISTINCT PATID) FROM FEAS1;
+SELECT DISTINCT PERSON_ID FROM G8;
+SELECT COUNT(DISTINCT PERSON_ID) FROM FEAS1;
 
 -- 2.5 Breakdowns
 DROP TABLE IF EXISTS G8BD;
 --Calculate age for all rows and add it as a column
 CREATE TEMPORARY TABLE G8BD as
-SELECT DISTINCT G8.PATID, G8.BIRTHDATETIME, G8.First_Vis, CAST(DATEDIFF(DAY,G8.BirthDateTime, g8.First_Vis)/365.25 AS INT)
-AS Age_1st_Vis, G8.SEX, g8.Race, G8.Ethnicity FROM G8;
+SELECT DISTINCT G8.PERSON_ID, G8.BIRTHDATETIME, G8.First_Vis, CAST(DATEDIFF(DAY,G8.BirthDateTime, g8.First_Vis)/365.25 AS INT)
+AS Age_1st_Vis, G8.GEnder, g8.Race, G8.Ethnicity FROM G8;
 -- null in age first vis? what to do
 
 -- Age
-WITH Age AS (SELECT DISTINCT PATID,
+WITH Age AS (SELECT DISTINCT PERSON_ID,
 CASE
 WHEN Age_1st_Vis is null THEN 'Other'
 WHEN Age_1st_Vis >= 18 AND Age_1st_Vis <40 THEN '18-39'
@@ -88,63 +102,57 @@ WHEN Age_1st_Vis >= 70 AND Age_1st_Vis <80 THEN '70-79'
 WHEN Age_1st_Vis >= 80 THEN '80+' ELSE 'Other'
 END
 AS Age_Cat FROM G8BD)
-SELECT Age_Cat, COUNT(DISTINCT PATID)
+SELECT Age_Cat, COUNT(DISTINCT PERSON_ID)
 FROM Age GROUP BY Age_Cat ORDER BY Age_Cat;
 
-SELECT SEX, COUNT(DISTINCT PATID) FROM G8 GROUP BY SEX ORDER BY SEX;
-SELECT Race, COUNT(DISTINCT PATID) FROM G8 GROUP BY Race ORDER BY Race;
-SELECT Ethnicity, COUNT(DISTINCT PATID) FROM G8 GROUP BY Ethnicity ORDER BY Ethnicity;
+SELECT Gender, COUNT(DISTINCT PERSON_ID) FROM G8 GROUP BY Gender ORDER BY Gender;
+SELECT Race, COUNT(DISTINCT PERSON_ID) FROM G8 GROUP BY Race ORDER BY Race;
+SELECT Ethnicity, COUNT(DISTINCT PERSON_ID) FROM G8 GROUP BY Ethnicity ORDER BY Ethnicity;
 /******************************** G_9 **********************************
             3. Number of patients from row 8 had prescription for metformin
 
 
 *************************************************************************/
 -- 3.1 Look Up Drugs/-- 3.2 Create Drug Dim Table for users over past 5 years
+DROP TABLE if exists metforminCodes;
+CREATE TEMPORARY TABLE metforminCodes as SELECT CONCEPT_ID, CONCEPT_NAME, DOMAIN_ID from OMOP_CDM.CDM.CONCEPT
+where CONCEPT_NAME like '%metformin%' or CONCEPT_NAME like '%Metformin%';
+select * from metforminCodes where domain_id != 'Drug';
 
-DROP TABLE if exists metformin;
-CREATE TEMPORARY TABLE metformin as SELECT PATID, RAW_RX_MED_NAME, RX_START_DATE, RX_END_DATE from PCORNET_CDM.CDM.prescribing
-where RAW_RX_MED_NAME like '%metformin%' or RAW_RX_MED_NAME like '%Metformin%';
-
-DROP TABLE if exists metformin_users;
-CREATE TEMPORARY TABLE metformin_users as
-select distinct PATID from metformin;
-select * from metformin_users;
-
-drop table if exists metformin5years;
-Create TEMPORARY table metformin5years as
-Select * from metformin where (RX_END_DATE >= CAST(DATEADD(YEAR, -5, GETDATE()) AS date) OR RX_END_DATE is null) AND
-RX_START_DATE < CAST(GETDATE() AS date) ;
-select * from metformin5years order by RX_END_DATE ASC;
-
-DROP TABLE IF EXISTS metformin_users5years;
-CREATE TEMPORARY TABLE metformin_users5years as
-select distinct PATID from metformin5years;
 
 -- 3.3 Identify Patients with diabetes on metformin
 DROP TABLE IF EXISTS G9;
 CREATE TEMPORARY TABLE G9 as
-SELECT DISTINCT G8.PATID, Race, ethnicity, sex, birthdatetime, first_vis
-FROM G8
-INNER JOIN metformin_users5years AS D
-ON G8.PATID = D.PATID;
-select * from G9;
+SELECT DISTINCT
+G8.PERSON_ID, Gender, Race,Ethnicity, BIRTHDATETIME, First_Vis, M.CONCEPT_NAME  FROM G8
+INNER JOIN
+OMOP_CDM.CDM.DRUG_EXPOSURE
+ON G8.PERSON_ID = OMOP_CDM.CDM.DRUG_EXPOSURE.PERSON_ID
+INNER JOIN metforminCodes AS M
+ON OMOP_CDM.CDM.DRUG_EXPOSURE.DRUG_CONCEPT_ID = M.CONCEPT_ID
+WHERE
+(OMOP_CDM.CDM.DRUG_EXPOSURE.DRUG_EXPOSURE_END_DATE >= CAST(DATEADD(YEAR, -5, GETDATE()) AS date) OR OMOP_CDM.CDM.DRUG_EXPOSURE.DRUG_EXPOSURE_END_DATE IS NULL) -- Start Date
+AND OMOP_CDM.CDM.DRUG_EXPOSURE.DRUG_EXPOSURE_START_DATE < CAST(GETDATE() AS date) -- End Date
+AND OMOP_CDM.CDM.DRUG_EXPOSURE.DRUG_EXPOSURE_START_DATE >= CAST('10/01/1999' AS date); -- Start of EHR
+select * from G9 order by First_Vis asc;
 
 -- 3.4 Save Row Cohort in FEAS2 and Get Distinct Patient Counts
 DROP TABLE IF EXISTS FEAS2;
 CREATE TEMPORARY TABLE FEAS2 as
-SELECT DISTINCT PATID FROM G9;
-SELECT COUNT(DISTINCT PATID) FROM FEAS2;
+SELECT DISTINCT PERSON_ID FROM G9;
+SELECT COUNT(DISTINCT PERSON_ID) FROM FEAS2;
 
 -- 3.5 Breakdowns
+-- this table exists for holding age
 DROP TABLE IF EXISTS G9BD;
 --Calculate age for all rows and add it as a column
 CREATE TEMPORARY TABLE G9BD as
-SELECT DISTINCT G9.PATID, G9.BIRTHDATETIME, G9.First_Vis, CAST(DATEDIFF(DAY,G9.BirthDateTime, g9.First_Vis)/365.25 AS INT)
-AS Age_1st_Vis, G9.SEX, g9.Race, G9.Ethnicity FROM G9;
+SELECT DISTINCT G9.PERSON_ID, G9.BIRTHDATETIME, G9.First_Vis, CAST(DATEDIFF(DAY,G9.BirthDateTime, g9.First_Vis)/365.25 AS INT)
+AS Age_1st_Vis, G9.GENDER, g9.Race, G9.Ethnicity FROM G9;
 -- null in age first vis? what to do
 
 -- Age
-WITH Age AS (SELECT DISTINCT PATID,
+WITH Age AS (SELECT DISTINCT PERSON_ID,
 CASE
 WHEN Age_1st_Vis is null THEN 'Other'
 WHEN Age_1st_Vis >= 18 AND Age_1st_Vis <40 THEN '18-39'
@@ -155,10 +163,10 @@ WHEN Age_1st_Vis >= 70 AND Age_1st_Vis <80 THEN '70-79'
 WHEN Age_1st_Vis >= 80 THEN '80+' ELSE 'Other'
 END
 AS Age_Cat FROM G9BD)
-SELECT Age_Cat, COUNT(DISTINCT PATID)
+SELECT Age_Cat, COUNT(DISTINCT PERSON_ID)
 FROM Age GROUP BY Age_Cat ORDER BY Age_Cat;
 
-SELECT SEX, COUNT(DISTINCT PATID) FROM G9 GROUP BY SEX ORDER BY SEX;
-SELECT Race, COUNT(DISTINCT PATID) FROM G9 GROUP BY Race ORDER BY Race;
-SELECT Ethnicity, COUNT(DISTINCT PATID) FROM G9 GROUP BY Ethnicity ORDER BY Ethnicity;
+SELECT Gender, COUNT(DISTINCT PERSON_ID) FROM G9 GROUP BY Gender ORDER BY Gender;
+SELECT Race, COUNT(DISTINCT PERSON_ID) FROM G9 GROUP BY Race ORDER BY Race;
+SELECT Ethnicity, COUNT(DISTINCT PERSON_ID) FROM G9 GROUP BY Ethnicity ORDER BY Ethnicity;
 /****************************************************************/
