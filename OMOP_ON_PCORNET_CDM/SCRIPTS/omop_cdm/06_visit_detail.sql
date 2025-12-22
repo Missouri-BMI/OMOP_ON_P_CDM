18251a2adf76b72cabededbea8e4385edf016a6a
Preston Hunter <ph8c8@umsystem.edu>
2025-12-22 17:07:52 -0600

    changed the care_site_id field in 05 and 06.
    filtered out null facilityid on 04, and use facilityid as care_site_id now. care_site name is concatenation of org_name and loc_nurse_unit (loc_nurse_unit helps with determining things like neonatal)

--- OMOP_ON_PCORNET_CDM/SCRIPTS/omop_cdm/06_visit_detail.sql
+++ OMOP_ON_PCORNET_CDM/SCRIPTS/omop_cdm/06_visit_detail.sql
@@ -43,7 +43,7 @@ SELECT
     {% else %}
         enc.providerid::INTEGER AS provider_id,
     {% endif %}
-    NULL::INTEGER AS care_site_id,
+    enc.facilityid::INTEGER               AS care_site_id,
     enc.raw_enc_type::VARCHAR(50)         AS visit_detail_source_value,
     0                                     AS visit_detail_source_concept_id,
     as_map.source_concept_id::INTEGER     AS admitted_from_concept_id, 
